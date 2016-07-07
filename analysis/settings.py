fb0544cfd7faaaaa9e9c16b5a32fae65a90a93ba
namco <namco1992@gmail.com>
2016-07-07 21:44:43 +0800

    modified gitignore

--- analysis/settings.py
+++
@@ -1,7 +0,0 @@
-# DB Settings
-LOCAL_URI = 'mongodb://localhost:27017/'
-MONGO_DATABASE = 'lagou_job_info'
-COLLECTION_NAME = 'python_20160614'
-
-# Keywords Settings
-OMITTING_WORDS = ['py', 'python', 'www', 'com', 'mac']
